50f9361e96202fab4287322e0e9aa3344fed4943
mohamed1408 <mohamedanastsi@gmail.com>
2021-09-24 09:29:18 +0530

    dasdasd

--- webrtc-handler.js
+++ webrtc-handler.js
@@ -1,6 +1,6 @@
 // const io = require("socket.io-client");
 var Peer = window.SimplePeer;
-var socket = io.connect("http://localhost:3000/");
+var socket = io.connect("https://wertc-server-simple.herokuapp.com/");
 
 var initiateBtn = document.getElementById('initiateBtn');
 var stopBtn = document.getElementById('stopBtn');
